b817807a9f341d59adc546982a35f7820b8d38d3
xi3236 <152358781+xi3236@users.noreply.github.com>
2023-12-05 12:29:56 +0800

    Update reptile_func_simplify.py
    Fixed some formatting issues

--- reptile_func_simplify.py
+++ reptile_func_simplify.py
@@ -12,11 +12,11 @@ def get_info(url, tag_name, class_name, process_type):
     if response.status_code == 200: # 检测网站状态
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        if process_type == 1: #爬取OPENAI_link
+        if process_type == 1: # 爬取OPENAI_link
             # 查找具有特定类名的<a>标签
             target_link = soup.find(tag_name, class_=class_name)
             if target_link:
-                # Combining base URL with the relative path
+                # 将基本 URL 与相对路径结合
                 post_url = url + target_link['href']
                 return post_url
             else:
@@ -38,23 +38,23 @@ def get_info(url, tag_name, class_name, process_type):
             else:
                 return None
 
-        if process_type == 4: # 爬取苹果博客link&title
+        if process_type == 4: # 爬取苹果博客link & title
             article = soup.select_one('h3.post-title a')
             apple_link = 'https://machinelearning.apple.com' + article['href']
             # 根据提供的HTML片段，定位到文章的标题和链接
             Apple_blog_title = article.text
             return  apple_link, Apple_blog_title
 
-        if process_type == 5:  # 爬取nvida link&title
+        if process_type == 5:  # 爬取nvida link & title
             target_link = "https://blogs.nvidia.com/ai-podcast/"
             target_text = "AI Podcast"
             ul_elems = soup.find_all('ul')
             all_links = []
-            # Extract links and texts from all <ul> elements
+            # 从所有 <ul> 元素中提取链接和文本
             for ul_elem in ul_elems:
                 links = [(link.get('href'), link.text) for link in ul_elem.find_all('a')]
                 all_links.extend(links)
-            # Extract the first link and text after the specified link-text pair
+            # 提取指定链接文本对后的第一个链接和文本
             found = False
             for link, text in all_links:
                 # print("text1 is", text)
@@ -63,7 +63,7 @@ def get_info(url, tag_name, class_name, process_type):
                 if link == target_link and text == target_text:
                     found = True
 
-        if process_type == 6:  # 爬取aws link&title
+        if process_type == 6:  # 爬取aws link & title
             articles = soup.find_all('div', class_='lb-col lb-mid-18 lb-tiny-24')
             if not articles:
                 print("No articles found.")
@@ -72,7 +72,7 @@ def get_info(url, tag_name, class_name, process_type):
             link = articles[0].find('a')['href']
             return  link, title
             
-        if process_type == 7:  # 爬取microsoft link&title
+        if process_type == 7:  # 爬取microsoft link & title
             # 由于网站可能有多个这样的链接，我们只选择第一个匹配的项
             link_element = soup.find(tag_name, class_=class_name)
             if link_element:
@@ -83,7 +83,7 @@ def get_info(url, tag_name, class_name, process_type):
                 return None, None
 
         # TODO --连接问题，timeout
-        if process_type == 8:  # 爬取techcrunch link&title
+        if process_type == 8:  # 爬取techcrunch link & title
             # 由于网站可能有多个这样的链接，我们只选择第一个匹配的项
             articles = soup.select('.post-block__title a')
             data_mrf_link, h_title = articles[0]['href'], articles[0].text
@@ -95,7 +95,7 @@ def get_info(url, tag_name, class_name, process_type):
         print(f"Failed to fetch the webpage. Status code: {response.status_code}")
         return None
 
-# 使用内置报的函数
+# 使用内置包的函数
 def get_arxiv_summary():
     search = arxiv.Search(
         query="AI, LLM, machine learning, NLP",
@@ -124,13 +124,13 @@ def fetch_gnews_links(query, language='en', country='US', period='1d', start_dat
                       exclude_websites=None):
     # Ensure that the exclude_websites parameter is a list
     content = {'title': [], 'summary': [], 'url': []}
-    # Initialize GNews
+    # 初始化 GNews
     google_news = GNews(language=language, country=country, period=period, start_date=start_date, end_date=end_date,
                         max_results=max_results, exclude_websites=exclude_websites)
-    # Fetch news based on the query
+    # 根据query获取新闻
     news_items = google_news.get_news(query)
     print(news_items)
-    # Extract URLs
+    # 提取URLs
     urls = [item['url'] for item in news_items]
     content['title'] = [item['title'] for item in news_items]
     for url in urls:
