2fe46003287eb3cbaf24a677b658262b03259ada
xi3236 <152358781+xi3236@users.noreply.github.com>
2023-12-08 17:33:48 +0800

    Update summarize_medai_news.py
    Add arxiv, language

--- summarize_medai_news.py
+++ summarize_medai_news.py
@@ -4,6 +4,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 import openai
+# TODO --在可以使用openai模型后可以删掉注释，以及后面的同名函数，并在调用函数时直接输入messages部分
 '''
 system_message = ('You are a very talented editor, skilled at consolidatingfragmented'
                   ' information and introductions into a cohesive script, without missing'
@@ -45,77 +46,90 @@ def get_completion_from_messages(messages,
     return response.choices[0].message["content"]
 '''
 
-# mock function
+# TODO --模拟函数可以跑通
 def summarize(url):
     summarized_content = "sum of " + url
     return summarized_content
 
-
-def completion(content_or_title, a='change ', b='language ', c='style'):
+def completion(content_or_title, a = 'change ', b = 'language ', c = 'style'):
     response = content_or_title + " " + a + b + c
     return response
 
+system_message = ["system_message_1: cohesive", "system_message_2: 3 points", "system_message_3: translate"]
 
-system_message = ["system_message_1", "system_message_2", "system_message_3"]
-
-# system_message和it should be content or title都是循环中要重新赋值的
-messages = [{'role': 'system',
-             'content': 'system_message'},
-            {'role': 'user',
-             'content': 'it should be content or title'}, ]
-
-
-# 生成每一个部分的summary
-def LLM_processing_content(news_items):
+# TODO --system_message和it should be content or title都是循环中要重新赋值的
+messages =  [           {'role':'system',
+                        'content': 'system_message'},
+                        {'role':'user',
+                        'content': 'it should be content or title'},]
+def LLM_processing_content(news_items, language):
     # summarize_website_content and translate information to Chinese
     for index, item in enumerate(news_items):
         # 归纳网页信息
         web_summarize = summarize(item['url'])
         # 在字典中新增sumarry的内容
         news_items[index]['web_summarize'] = web_summarize
+        if language == 'English':
+            if language == 'Chinese':
+                # TODO --这里还要在循环里修改message，有点复杂，但是不修改就要每次重新生成一个
+                # 提取messsage中内容部分翻译网页信息为中文
+                # 中文翻译循环中修改messages中的系统提示为system_message_3
+                messages[0]['content'] = system_message[2]
+                # 同时修改messages中的内容部分
+                messages[1]['content'] = web_summarize
+                # 实际上最后输入是messages这个列表, 但是使用前要注意修改messages中的两部分信息
+                web_chinese = completion(messages[1]['content']) # TODO --最后输入实际是messages这个列表
+                # 新增字典
+                news_items[index]['web_summarize_chinese'] = web_chinese
 
-        language = 'chinese'  # 这里先设成生成中文了
-        if language == 'chinese':
-            # TODO --这里还要循环修改message，有点复杂，但是不修改就要每次重新生成一个
-            # 提取messsage中内容部分翻译网页信息为中文
-            # 中文翻译循环中修改messages中的系统提示为system_message_3
-            messages[0]['content'] = system_message[2]
-            # 同时修改messages中的内容部分
-            messages[1]['content'] = web_summarize
-            # 实际上最后输入是messages这个列表, 但是使用前要注意修改messages中的两部分信息
-            web_chinese = completion(messages[1]['content'])  # TODO --最后输入实际是messages这个列表
-            # 新增字典
-            # TODO --或许转换成中文不应该同步生成，可能会很慢
-            news_items[index]['web_summarize_chinese'] = web_chinese
-
-            # TODO --这里中文的部分还要再看看是直接归纳中文信息，还是直接翻译归纳后的
-            # 提取messsage中标题部分翻译网页信息为中文
-            messages[1]['content'] = news_items[index]['title']
-            title_chinese = completion(messages[1]['content'])
-            news_items[index]['title_chinese'] = title_chinese
+                # TODO --这里中文的部分还要再看看是直接归纳中文信息，还是直接翻译归纳后的
+                # 提取messsage中标题部分翻译网页信息为中文
+                messages[1]['content'] = news_items[index]['title']
+                title_chinese = completion(messages[1]['content'])
+                news_items[index]['title_chinese'] = title_chinese
+            return news_items
     return news_items
-
-
-# 生成整个日报的summary
-def generate_paper_summary(news_items):
+def generate_paper_summary(news_items, language):
     # 归纳摘要前修改messages中的系统提示为system_message
     messages[0]['content'] = system_message[0]
     # 同时修改messages中的内容部分为全部网页的summary
     summary_whole = [item['web_summarize'] for item in news_items]
     messages[1]['content'] = summary_whole
-    generate_paper_summary = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
-    # test
-    language = 'English'
+    generate_paper_summary = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
+
     if language == 'English':
         # 生成key points前修改messages中的系统提示为system_message_2
         messages[0]['content'] = system_message[1]
         # 输入应为上一段整理好的信息
         messages[1]['content'] = generate_paper_summary
-        generate_key_points = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
-    if language == 'Chinese':
+        generate_key_points = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
+    elif language == 'Chinese':
         # 生成key points前修改messages中的系统提示为system_message_2
-        messages[0]['content'] = system_message[3]
+        messages[0]['content'] = system_message[2]
         # 输入应为上一段整理好的信息
         messages[1]['content'] = generate_paper_summary
-        generate_key_points = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
+        generate_key_points = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
     return generate_key_points
+
+# 收集并直接生成arxiv的summary
+import arxiv
+arxiv_items = [ ]
+def get_arxiv_summary(language, max_results ):
+    search = arxiv.Search(
+        query="AI, LLM, machine learning, NLP",
+        # max_results=st.session_state.arxiv,
+        max_results=max_results,
+        sort_by=arxiv.SortCriterion.SubmittedDate
+    )
+    for index, result in enumerate(search.results()):
+        new_paper = {'paper from': "arxiv", 'paper_summary': result.summary, 'paper_title': result.title}
+        arxiv_items.append(new_paper)
+        if language == 'English':
+            pass
+        elif language == 'Chinese':
+            new_paper_summary_Chinese = completion(new_paper['paper_summary'])
+            new_paper_title_Chinese = completion(new_paper['paper_title']) # 这里需要用system_message_3
+            # 新增字典
+            arxiv_items[index]['paper_summarize_Chinese'] = new_paper_summary_Chinese
+            arxiv_items[index]['paper_title_Chinese'] = new_paper_title_Chinese
+    return arxiv_items
