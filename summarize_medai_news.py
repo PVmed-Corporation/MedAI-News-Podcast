adb3c14c0161ec3c74177fc4a25f2dc80fd0dd6f
xi3236 <152358781+xi3236@users.noreply.github.com>
2023-12-08 13:44:04 +0800

    Update summarize_medai_news.py
    modify the problem of comment

--- summarize_medai_news.py
+++ summarize_medai_news.py
@@ -4,26 +4,19 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 import openai
-
 '''
-# It tries to split on them in order until the chunks
-# are small enough. The default list is ["\n\n", "\n", " ", ""].
-system_message = '''
-                You are a very talented editor, skilled at consolidating 
-                fragmented information and introductions into a cohesive script, without missing any details.
-                Compile the news article based on the information in 【】.  
-                '''
+system_message = ('You are a very talented editor, skilled at consolidatingfragmented'
+                  ' information and introductions into a cohesive script, without missing'
+                  ' any details. Compile the news article based on the information in 【】. ')
 
-system_message_2 = '''
-                You are a linguist, skilled in summarizing textual content and presenting it in 3 bullet points using markdown. 
-                '''
+system_message_2 = ('You are a linguist, skilled in summarizing textual content and presenting'
+                    ' it in 3 bullet points using markdown. ')
 
-system_message_3 = '''
-                你是个语言学家，擅长把英文翻译成中文。要注意表达的流畅和使用中文的表达习惯。不要返回多余的信息，只把文字翻译成中文。
-                '''
-# language model
-def summarize(url, temperature=1, model_name="gpt-3.5-turbo-16k", chain_type="stuff"):
-# TODO --查函数参数
+system_message_3 = ('你是个语言学家，擅长把英文翻译成中文。要注意表达的流畅和使用中文的表达习惯。'
+                    '不要返回多余的信息，只把文字翻译成中文。')
+
+# 语言模型
+def summarize_website_content(url, temperature=1, model_name="gpt-3.5-turbo-16k", chain_type="stuff"):
     if True:
         # Load the content from the given URL
         loader = WebBaseLoader(url)
@@ -38,8 +31,9 @@ def summarize(url, temperature=1, model_name="gpt-3.5-turbo-16k", chain_type="st
     else:
         return 'No result'
 
-#  generate responses based on the provided context. 
-def completion(messages,
+# 对话模型
+#  generate responses based on the provided context. 可以输入sys，human，ai
+def get_completion_from_messages(messages,
                                  model="gpt-3.5-turbo-16k",
                                  temperature=1.5, max_tokens=7000):
     response = openai.ChatCompletion.create(
@@ -56,17 +50,20 @@ def summarize(url):
     summarized_content = "sum of " + url
     return summarized_content
 
-def completion(content_or_title, a = 'change ', b = 'language ', c = 'style'):
+
+def completion(content_or_title, a='change ', b='language ', c='style'):
     response = content_or_title + " " + a + b + c
     return response
+
 
 system_message = ["system_message_1", "system_message_2", "system_message_3"]
 
 # system_message和it should be content or title都是循环中要重新赋值的
-messages =  [           {'role':'system',
-                        'content': 'system_message'},
-                        {'role':'user',
-                        'content': 'it should be content or title'},]
+messages = [{'role': 'system',
+             'content': 'system_message'},
+            {'role': 'user',
+             'content': 'it should be content or title'}, ]
+
 
 # 生成每一个部分的summary
 def LLM_processing_content(news_items):
@@ -77,7 +74,7 @@ def LLM_processing_content(news_items):
         # 在字典中新增sumarry的内容
         news_items[index]['web_summarize'] = web_summarize
 
-        language = 'chinese' # 这里先设成生成中文了
+        language = 'chinese'  # 这里先设成生成中文了
         if language == 'chinese':
             # TODO --这里还要循环修改message，有点复杂，但是不修改就要每次重新生成一个
             # 提取messsage中内容部分翻译网页信息为中文
@@ -86,11 +83,11 @@ def LLM_processing_content(news_items):
             # 同时修改messages中的内容部分
             messages[1]['content'] = web_summarize
             # 实际上最后输入是messages这个列表, 但是使用前要注意修改messages中的两部分信息
-            web_chinese = completion(messages[1]['content']) # TODO --最后输入实际是messages这个列表
+            web_chinese = completion(messages[1]['content'])  # TODO --最后输入实际是messages这个列表
             # 新增字典
             # TODO --或许转换成中文不应该同步生成，可能会很慢
             news_items[index]['web_summarize_chinese'] = web_chinese
-    
+
             # TODO --这里中文的部分还要再看看是直接归纳中文信息，还是直接翻译归纳后的
             # 提取messsage中标题部分翻译网页信息为中文
             messages[1]['content'] = news_items[index]['title']
@@ -98,15 +95,15 @@ def LLM_processing_content(news_items):
             news_items[index]['title_chinese'] = title_chinese
     return news_items
 
+
 # 生成整个日报的summary
 def generate_paper_summary(news_items):
-
     # 归纳摘要前修改messages中的系统提示为system_message
     messages[0]['content'] = system_message[0]
     # 同时修改messages中的内容部分为全部网页的summary
     summary_whole = [item['web_summarize'] for item in news_items]
     messages[1]['content'] = summary_whole
-    generate_paper_summary = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
+    generate_paper_summary = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
     # test
     language = 'English'
     if language == 'English':
@@ -114,11 +111,11 @@ def generate_paper_summary(news_items):
         messages[0]['content'] = system_message[1]
         # 输入应为上一段整理好的信息
         messages[1]['content'] = generate_paper_summary
-        generate_key_points = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
+        generate_key_points = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
     if language == 'Chinese':
         # 生成key points前修改messages中的系统提示为system_message_2
         messages[0]['content'] = system_message[3]
         # 输入应为上一段整理好的信息
         messages[1]['content'] = generate_paper_summary
-        generate_key_points = completion(str(messages[1]['content'])) # TODO --最后输入实际是messages这个列表
+        generate_key_points = completion(str(messages[1]['content']))  # TODO --最后输入实际是messages这个列表
     return generate_key_points
