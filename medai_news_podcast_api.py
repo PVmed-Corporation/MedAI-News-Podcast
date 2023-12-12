3cab4f98880b9c42c927738716c9e8930b6d9efa
xi3236 <152358781+xi3236@users.noreply.github.com>
2023-12-12 15:57:40 +0800

    Update medai_news_podcast_api.py
    LLM_processing_content is ok

--- medai_news_podcast_api.py
+++ medai_news_podcast_api.py
@@ -1,5 +1,5 @@
-from get_medai_news import get_websit_info, get_arxiv_summary, get_youtube_dojo, fetch_gnews_links
-from summarize_medai_news import summarize, completion, LLM_processing_content, generate_paper_summary, get_arxiv_summary
+from get_medai_news import get_websit_info, get_youtube_dojo, fetch_gnews_links #  get_arxiv_summary,
+from summarize_medai_news_test import summarize, completion, LLM_processing_content, generate_paper_summary # get_arxiv_summary
 import pprint
 def medai_news_podcast_api(websites, language):
     # 1. collect the information
@@ -15,8 +15,8 @@ def medai_news_podcast_api(websites, language):
     # import pdb; pdb.set_trace()
 
     # arxiv直接生成summary
-    arxiv_summary = get_arxiv_summary(language, max_results=3) # max_results可以自由改动
-    pprint.pprint(arxiv_summary)
+    # arxiv_summary = get_arxiv_summary(language, max_results=3) # max_results可以自由改动
+    # pprint.pprint(arxiv_summary)
 
     '''
     # # TODO -- 连接问题，timeout
@@ -33,7 +33,7 @@ def medai_news_podcast_api(websites, language):
     # 2. summarize the content
     # 生成每篇文章的summary
     LLM_content = LLM_processing_content(news_items, language)
-    print("LLM_content:", LLM_content)
+    print("LLM_content:", LLM_content, '\n')
 
     # 生成整个小报的summary
     LLM_paper_summary = generate_paper_summary(LLM_content, language)
@@ -52,15 +52,14 @@ if __name__ == '__main__':
     # 如果链接太多会 too many values to unpack (expected 2)
     websites = [
         WebsiteInfo(url="https://machinelearning.apple.com/", tag_name="h3.post-title a", class_name="", process_type="apple"), # apple_link&title
-        WebsiteInfo(url="https://blogs.nvidia.com/ai-podcast/", tag_name="ul", class_name="AI Podcast",process_type="nvidia"), # nvida_link&title
+        # WebsiteInfo(url="https://blogs.nvidia.com/ai-podcast/", tag_name="ul", class_name="AI Podcast",process_type="nvidia"), # nvida_link&title
         WebsiteInfo(url="https://aws.amazon.com/blogs/machine-learning/", tag_name="div", class_name="lb-col lb-mid-18 lb-tiny-24", process_type="aws"), # aws
         WebsiteInfo(url="https://blogs.microsoft.com/", tag_name="a", class_name="f-post-link", process_type="microsoft"), # microsoft
         WebsiteInfo(url="https://openai.com/", tag_name="a", class_name="ui-link group relative cursor-pointer", process_type="openai"), # openai_link
-        WebsiteInfo(url="https://lexfridman.com/podcast/", tag_name="a", class_name="", process_type="lexfridman"), # lexfridman_link
+        # WebsiteInfo(url="https://lexfridman.com/podcast/", tag_name="a", class_name="", process_type="lexfridman"), # lexfridman_link
         # TODO --连接问题，timeout
-        # WebsiteInfo(url="https://techcrunch.com/category/artificial-intelligence/", tag_name="", class_name="", process_type="techcrunch"), # techcrunch频道
+        WebsiteInfo(url="https://techcrunch.com/category/artificial-intelligence/", tag_name="", class_name="", process_type="techcrunch"), # techcrunch频道
     ]
 
     medai_news_podcast_api(websites, 'Chinese') # language可以选择Chinese或English
-
 
